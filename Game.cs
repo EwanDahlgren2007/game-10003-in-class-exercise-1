41682ed0a3a976a062b7a7e2049ba196256f62ae
ewand <ewand@EWANS_LAPTOP>
2025-09-26 15:50:11 -0400

    now there's code :D

--- Game.cs
+++ Game.cs
@@ -1,32 +1,38 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
+using System.IO;
 using System.Numerics;
 
 // The namespace your code is in.
 namespace MohawkGame2D
 {
-    /// <summary>
-    ///     Your game code goes inside this class!
-    /// </summary>
+
     public class Game
     {
-        // Place your variables here:
-
-
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
+        
         public void Setup()
         {
-
+            Window.SetTitle("Loop Test");
+            Window.SetSize(400, 400);
+            Window.TargetFPS = 60;
         }
 
-        /// <summary>
-        ///     Update runs every frame.
-        /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.White);
+            for (int i = 0; i < 10; i++)
+            {
+                Draw.FillColor = Color.Black;
+                Draw.LineSize = 0;
 
+                float offsetX = Input.GetMouseX();
+                offsetX -= 180;
+                float offsetY = Input.GetMouseY();
+
+                float x = i * 40.0f;
+
+                Draw.Circle(x+offsetX, offsetY, 10);
+            }
         }
     }
 
